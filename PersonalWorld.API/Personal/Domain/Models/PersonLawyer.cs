eebcbbddb13175bd8dc0c8a7f553c9b185db3b4d
JoaquinSM13 <u201910511@upc.edu.pe>
2022-06-05 08:54:49 -0500

    feat: person lawyer entity

---
+++ PersonalWorld.API/Personal/Domain/Models/PersonLawyer.cs
@@ -0,0 +1,10 @@
+namespace PersonalWorld.API.Personal.Domain.Models;
+
+public class PersonLawyer : Person
+{
+    public string Specialty { get; set; }
+    public int WonCases { get; set; }
+    public int TotalCases { get; set; }
+    public int LostCases { get; set; }
+    public string Attribute { get; set; }
+}
