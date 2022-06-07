89e862215dd173581aa531d6f09c24db11d9bf7a
djalmadioses <64975820+DamianDTM@users.noreply.github.com>
2022-06-06 22:10:11 -0500

    feat: plan resource and save plan resource
    add plan resource and save plan resource in the project

---
+++ PersonalWorld.API/Personal/Resources/PlanResource.cs
@@ -0,0 +1,9 @@
+namespace PersonalWorld.API.Personal.Resources;
+
+public class PlanResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+}
